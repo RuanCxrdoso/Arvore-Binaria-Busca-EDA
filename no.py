cc7ac3b81563a7d19d6d1b5ae544475e08fec0d5
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2023-11-21 13:31:45 -0300

    feat: Binary Tree Stack and Node class algorithm

---
+++ no.py
@@ -0,0 +1,5 @@
+class No():
+  def __init__(self):
+    self.valor = None
+    self.dir = None
+    self.esq = None
---
+++ pilha.py
@@ -0,0 +1,24 @@
+class Pilha():
+  def __init__(self):
+    self.pilha = [0] * 1000
+    self.max = 1000
+    self.topo = -1
+
+  def empilhar(self, chave):
+    if self.topo < self.max:
+      self.topo += 1
+      self.pilha[self.topo] = chave
+    else:
+       print('Pilha cheia !')
+
+  def desempilhar(self):
+    chave = 0
+    if self.topo >= 0:
+       chave = self.pilha[self.topo]
+       self.topo -= 1
+       return chave
+    else:
+       print('Pilha vazia !')
+
+  # def __str__(self):
+  #       return "[" + ", ".join(map(str, self.pilha)) + "]"
